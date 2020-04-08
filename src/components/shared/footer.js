2d43e4dd32b9a38b26c76cf604cc2c9e3006a24d
merijnlioen <merijnlioen@gmail.com>
2020-04-08 23:10:25 +0200

    More footer stuff

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Facebook, Instagram, LinkedIn, Github } from './icons'
 
 const Footer = () => (
     <footer className="footer">
         <div className="inner">
+            <nav className="footer__nav">
+                <Facebook />
+                <Instagram />
+                <LinkedIn />
+            </nav>
             <p className="copyrights">&copy; 2020 ShareTune. All rights reserved.</p>
         </div>
     </footer>
