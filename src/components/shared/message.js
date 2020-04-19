eed35dc5a5589a21cb55f9a6171a6328357052f1
merijnlioen <merijnlioen@gmail.com>
2020-04-19 13:55:06 +0200

    Close message after 5 seconds

--- src/components/shared/message.js
+++ src/components/shared/message.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { updateIsMessageOpened } from '../../actions/global-actions'
 import { Close } from '../shared/icons'
 
-const Message = ({ message, updateIsMessageOpened, isMessageOpen }) => (
-    <CSSTransition
-        in={message && isMessageOpen}
-        timeout={1000}
-        classNames="fade"
-        unmountOnExit
-        appear={true}
-    >
-        <div className="message__container">
-            <div className="message">{message}</div>
-            <div className="close" onClick={() => updateIsMessageOpened(false)}><Close /></div>
-        </div>
-    </CSSTransition>
-)
+const Message = ({ message, updateIsMessageOpened, isMessageOpen }) => {
+    useEffect(() => {
+        if (!updateIsMessageOpened) return
+
+        setTimeout(() => {
+            updateIsMessageOpened(false)
+        }, 5000)
+    }, [updateIsMessageOpened, message])
+
+    return (
+        <CSSTransition
+            in={message && isMessageOpen}
+            timeout={1000}
+            classNames="fade"
+            unmountOnExit
+            appear={true}
+        >
+            <div className="message__container">
+                <div className="message">{message}</div>
+                <div className="close" onClick={() => updateIsMessageOpened(false)}><Close /></div>
+            </div>
+        </CSSTransition>
+    )
+}
 
 const mapStateToProps = state => ({
     message: state.global.message,
