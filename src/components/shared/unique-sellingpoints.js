3ba1c4585001d64a8f9fb8ab3eb9cd63686443e8
Merijn Lioen <merijnlioen@Merijns-MacBook-Pro.local>
2020-09-16 19:04:55 +0200

    USP and mobile fixes

--- src/components/shared/unique-sellingpoints.js
+++ src/components/shared/unique-sellingpoints.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Logo from './logo'
+import People from 'react-ionicons/lib/IosPeopleOutline'
+import HeadPhones from 'react-ionicons/lib/IosHeadsetOutline'
+import Note from 'react-ionicons/lib/IosMusicalNoteOutline'
 
 const UniqueSellingpoints = () => (
     <div className="usps">
         <span className="heading">Why </span><Logo /><span className="heading">?</span>
         <div className="usp__container">
             <div className="usp">
-                <div className="usp__image" />
+                <div className="usp__image">
+                    <Note fontSize="50px" />
+                </div>
+
+                <h4 className="subheading usp__title">Share</h4>
+                <p className="text">
+                    Share your favorite tunes with the outside world,
+                    so your family, friends and even strangers can enjoy
+                    your outstanding taste in music.
+                </p>
+            </div>
+
+            <div className="usp">
+                <div className="usp__image">
+                    <People fontSize="50px" />
+                </div>
+
                 <h4 className="subheading usp__title">Interaction</h4>
                 <p className="text">
                     Comment on your favorite tunes and have a chat with
@@ -16,21 +35,14 @@ const UniqueSellingpoints = () => (
             </div>
 
             <div className="usp">
-                <div className="usp__image" />
+                <div className="usp__image">
+                    <HeadPhones fontSize="50px" />
+                </div>
+
                 <h4 className="subheading usp__title">Enjoy</h4>
                 <p className="text">
                     Enjoy some of your favorite tunes uploaded by your
                     friends, family, and even strangers!
-                </p>
-            </div>
-            
-            <div className="usp">
-                <div className="usp__image" />
-                <h4 className="subheading usp__title">Share</h4>
-                <p className="text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras rutrum nunc magna, vel luctus justo interdum ac.
-                    Vestibulum lobortis ipsum ante, vel ultricies quam tincidunt ac.
                 </p>
             </div>
         </div>
